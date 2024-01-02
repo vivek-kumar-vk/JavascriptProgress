9bbc01e37c92a8d67169b78197cbee1561d08456
Vivek Kumar <vkjha2511@gmail.com>
2024-01-02 18:32:24 +0530

    Done with fundamental1

---
+++ Fundamental-1/starter/script.js
@@ -0,0 +1,47 @@
+let js = "amazin";
+console.log(10+12+34-67);
+
+console.log("Jonas");
+
+let firstNmae=  "Vivek";
+console.log(firstNmae);
+
+let country = "India";
+let continent = "ASia";
+let population =31000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
+console.log(true);
+let javaScript = true;
+console.log(javaScript);
+
+console.log(typeof javaScript);
+
+javaScript = "yes";
+console.log(javaScript);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 91;
+console.log(typeof year);
+console.log(typeof null);
+
+//let const var
+//let use to change value latter during coding or to mutate the variable
+
+let age = 30;
+age = 31;
+
+//const are usr to make value constant and can't be changed throught out the code or ever that is mutable
+
+const birth = 65;
+
+//var keytword should be avoide but are use in legacy codebases
+
+//operators
+
