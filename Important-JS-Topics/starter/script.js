4e79e08ded3cd82a5b668ab26923981128f7ef9b
Vivek Kumar <vkjha2511@gmail.com>
2024-01-09 19:15:23 +0530

    till this keyword

---
+++ Important-JS-Topics/starter/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2024 - birthYear;
+//   console.log(firstName);
+
+//   function printAge() {
+//     const output = `your age is ${age}`;
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'vivek';
+// calcAge(1999);
+
+//HOISTING
+//hoisting with var
+console.log(me);
+// console.log(job);
+// console.log(year);
+var me = 'Vivek';
+let job = 'teacher';
+const year = 1999;
+
+//hoisting with function
